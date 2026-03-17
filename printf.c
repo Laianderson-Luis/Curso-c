047d608b8e6ae976df96c706fd79890c883b5f23
Laianderson <luislaianderson@gmail.com>
2026-03-16 23:20:51 -0300

    Aula 01: Adicionado esqueleto basico e Hello World

---
+++ printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h> /* Bibllioteca padrão input\otput  */
+
+int main () {  /* Função principal do código */
+
+    printf ("Ola Mundo\n"); /* Vai imprimir mensagem na tela e pular uma linha*/
+    printf ("Linha pulada");
+
+
+
+    return 0; /*Retorna 0 para indicar que o sistema operacional terminou com sucesso*/
+}
