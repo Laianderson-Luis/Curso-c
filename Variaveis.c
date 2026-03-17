ae7dcdd5f64778a2de02606ee5ef1db96ffa2f3c
Laianderson <luislaianderson@gmail.com>
2026-03-17 18:11:52 -0300

    Variaveis

---
+++ Variaveis.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()  {
+
+  int idade = 20;  /* Declarção da variável do tio inteira chamada idade que vale 20*/
+  int maeIdade = 50;
+  int paiIdade = 50;
+   printf ("Minha idade e %i\n Idade da mae  e %i\n Idade do pai  e %i\n ", idade, maeIdade,paiIdade); /* O %i será substituido pelo valor da variavel idade*/
+
+
+    return 0;
+}
