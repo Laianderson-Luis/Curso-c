db56aad4dd8601da177162a48e1e3be3189c0a62
Laianderson <luislaianderson@gmail.com>
2026-03-17 21:48:54 -0300

    Variaveis tpoo float, int e char

---
+++ Variaveis tipoo float e char.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main () {
+
+     // Declaração de variaveis  do tipo inteiro, decimal e caractere
+
+   int inteira = 10;
+   float decimal = 22.0;
+   char caractere = 'a';
+ 
+
+  printf ("%i\n", inteira);
+  printf ("%f\n", decimal);
+  printf ("%c\n", caractere);
+
+    return 0;
+}
